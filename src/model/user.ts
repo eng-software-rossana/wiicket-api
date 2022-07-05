2aaf7cb3524653aa96966ac1ce44653430105c67
Eantropix <eantropixdev@outlook.com>
2022-07-04 23:32:41 -0300

    Stringify hell

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,31 +1,53 @@
 import { PrismaClient } from '@prisma/client'
+import { randomUUID } from 'crypto'
+import { type } from 'os'
 
 const prisma = new PrismaClient()
+export class User {
+    id: Number
+    user_id: String
+    name: String
+    login: String
+    password: String
 
-let userId : string = ""
+    async createUser(createUserRequest: String) {
+        console.log(createUserRequest)
+        let createUserJson : User = JSON.parse(JSON.stringify(createUserRequest));
+        // let createUserJson = createUserRequest
+        const user = await prisma.user.create({
+            data: {
+                name: createUserJson.name.toString(),
+                user_id: randomUUID(),
+                login: createUserJson.login.toString(),
+                password: createUserJson.password.toString()
+            }
+        })
+        console.log("user: ", user);
+        console.log("type of user: ", typeof(user));
+        console.log("stringify: ", JSON.stringify(user));
+        console.log("type of stringify: ", typeof(JSON.stringify(user)))
+        return JSON.parse(JSON.stringify(user));
+    }
+}
 
 async function getUser(userId: string) {
     const user = await prisma.user.findUnique({
         where: {
             user_id: userId
         }
-    })
-    if (user == null) { throw new Error }
+    });
+    if (user == null) { throw new Error; }
+    return user;
 }
 
-async function createUser(userInfo: JSON) {
-    userInfo = userInfo.parse
-    const user = await prisma.user.create({
-        data: {
-            name: userInfo["name"]
-        }
-    })
-}
 
-getUser(userId)
-    .catch((ex) => {
-        throw ex
-    })
-    .finally(async () => {
-        await prisma.$disconnect()
-    })
+
+// getUser(userId)
+//     .catch((ex) => {
+//         throw ex
+//     })
+//     .finally(async () => {
+//         await prisma.$disconnect()
+//     })
+
+export default User;
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,17 @@
-import { Router } from "express";
+import { response, Router } from "express";
+import { User } from "./model/user";
 
 const routes = Router();
+let user = new User();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: "Hello World" });
-})
+routes.get("/", (req, res) => {
+  return res.json({ message: "Hello World" });
+});
+routes.post("/user", (req, res) => {
+//   let result = user.createUser(req.body).finally(() => { return JSON.parse(res.body) })
+  let result = user.createUser(req.body).then((res) => { return true })
+  console.log("esse é o result: ", result);
+  console.log("saimos");
+});
 
 export default routes;
